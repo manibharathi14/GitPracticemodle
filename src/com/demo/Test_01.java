5fd3ac1e5cdda8b48e52b97f89cd86b18ac61b3b
Personal <Personal@DESKTOP-RJ6N41P>
2023-09-14 14:35:42 +0530

    First commit...

---
+++ src/com/demo/Test_01.java
@@ -0,0 +1,26 @@
+package com.demo;
+
+public class Test_01 {
+	
+	
+	public void studant_name() {
+		System.out.println("John");
+		
+	}
+	public void college() {
+		System.out.println("SRM");
+		
+	}
+	public void city() {
+		System.out.println("Chennai");
+		
+	}
+	
+	public static void main(String[] args) {
+		Test_01 t = new Test_01();
+		t.studant_name();
+		t.college();
+		t.city();
+	}
+
+}
